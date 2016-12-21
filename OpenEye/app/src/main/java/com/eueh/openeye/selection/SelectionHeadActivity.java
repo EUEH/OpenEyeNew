2915b8742221b765183dd2a272983696834e592b
顾彬 <1107593449@qq.com>
2016-12-21 19:54:21 +0800

    再次分发任务之前的准备

---
+++ OpenEye/app/src/main/java/com/eueh/openeye/selection/SelectionHeadActivity.java
@@ -0,0 +1,26 @@
+package com.eueh.openeye.selection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eueh.openeye.R;
+import com.eueh.openeye.base.BaseActivity;
+
+public class SelectionHeadActivity extends BaseActivity {
+
+
+    @Override
+    public int setLayout() {
+        return R.layout.activity_selection_head;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
