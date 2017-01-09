c6828a61511265d5d8e008200ce0a16d4039176b
顾彬 <1107593449@qq.com>
2017-01-09 14:20:11 +0800

    123

--- OpenEye/app/src/main/java/com/eueh/openeye/MainActivity.java
+++ OpenEye/app/src/main/java/com/eueh/openeye/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 
 import com.eueh.openeye.base.BaseActivity;
@@ -14,6 +16,9 @@ import com.eueh.openeye.downloadservice.DownLoadService;
 import com.eueh.openeye.find.FindFragment;
 import com.eueh.openeye.mine.MineFragment;
 import com.eueh.openeye.selection.selection_main.SelectionFragment;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     private FrameLayout frameLayout;
@@ -39,6 +44,45 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         rbtFind.setOnClickListener(this);
 
         replace(new SelectionFragment());
+
+        //悬浮按钮
+        ImageView icon = new ImageView(this);
+        icon.setImageResource(R.mipmap.photome);
+
+        FloatingActionButton actionButton = new FloatingActionButton
+                .Builder(this).setContentView(icon).build();
+
+        actionButton.setScaleX(0.6f);
+        actionButton.setScaleY(0.6f);
+        actionButton.setX(-1f);
+        actionButton.setY(-60f);
+
+
+
+        SubActionButton.Builder itemBuilder = new
+                SubActionButton.Builder(this);
+
+        ImageView itemIconPhoto = new ImageView(this);
+        ImageView itemIconSay = new ImageView(this);
+        ImageView itemIconFind = new ImageView(this);
+
+
+        itemIconPhoto.setImageResource(R.mipmap.email_filling);
+        itemIconSay.setImageResource(R.mipmap.sayme);
+        itemIconFind.setImageResource(R.mipmap.findme);
+
+        SubActionButton buttonPhoto = itemBuilder.setContentView(itemIconPhoto).build();
+        SubActionButton buttonSay = itemBuilder.setContentView(itemIconSay).build();
+        SubActionButton buttonFind = itemBuilder.setContentView(itemIconFind).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(buttonPhoto)
+                .addSubActionView(buttonSay)
+                .addSubActionView(buttonFind)
+                .attachTo(actionButton)
+                .build();
+
+
     }
 
     @Override
@@ -65,7 +109,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.rbt_selection:
                 replace(new SelectionFragment());
                 break;
